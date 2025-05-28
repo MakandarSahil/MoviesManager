cbf74f8569b451aeb5aabc02fc2044cc68004037
MakandarSahil <sahilmakandar15@gmail.com>
2025-05-28 19:28:05 +0530

    tailwind / icon

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <View>
+      <Text>this commit has tailwind setup which need improvment and icon for android and splash screen for android</Text>
+      <Text className='text-white text-sm'>This is my Movie Manager App</Text>
+    </View>
+  )
+}
+
+export default Hero
